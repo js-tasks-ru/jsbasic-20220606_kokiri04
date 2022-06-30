0c82ffa332c776a0b59832da512b0f71c8d41668
kokiri04 <kokiri04@gmail.com>
2022-07-01 00:12:18 +0300

    описание того, что вы сделали

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  sum () {
+    return this.a + this.b;
+  },
+  mul () {
+    return this.a * this.b;
+  },
+  read () {
+    this.a = +prompt('a?');
+    this.b = +prompt('b?');
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
